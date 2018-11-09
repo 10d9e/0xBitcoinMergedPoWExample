c6a51f43c5f50d3203e8101ba73a5aee9bfe90bb
Jay Logelin <jason.logelin@virtustream.com>
2018-11-08 20:30:38 -0400

    added check for submitted solutions

--- index.js
+++ index.js
@@ -31,9 +31,19 @@ let MAX_TARGET = web3.utils.toBN( 2 ).pow( web3.utils.toBN( 234 ) )
 // the minimum Proof of work difficulty required to secure this service
 let SERVICE_MINIMUM_DIFFICULTY = 65536
 
+// set to keep track of submitted solutions
+// TODO persist this with a db
+const submittedSolutions = new Set();
+
 // an arbitrary REST service method that is secured by 0xBitcoin proof of work
 // curl -d '{ "nonce":"0xdeadbeef", "origin": "0xaddress"}' -H "Content-Type: application/json" http://127.0.0.1:8080/example2/execute
 app.post('/example2/execute', asyncMiddleware( async (request, response, next) => {
+
+	// make sure repeated solutions are not submitted
+	if(submittedSolutions.has(request.body.nonce)){
+		throw 'Solution has already been submitted'
+	}
+	submittedSolutions.add(request.body.nonce)
 
 	var challenge = await bitcoin.getChallengeNumber(contractAddress)
 
@@ -59,6 +69,12 @@ app.post('/example2/execute', asyncMiddleware( async (request, response, next) =
 // an arbitrary REST service method that is secured by 0xBitcoin proof of work
 // curl -d '{ "nonce":"0xdeadbeef", "origin": "0xaddress", "payload": "{ }"}' -H "Content-Type: application/json" http://127.0.0.1:8080/example1/execute
 app.post('/example1/execute', asyncMiddleware( async (request, response, next) => {
+
+	// make sure repeated solutions are not submitted
+	if(submittedSolutions.has(request.body.nonce)){
+		throw 'Solution has already been submitted'
+	}
+	submittedSolutions.add(request.body.nonce)
 
 	// merge with 0xbitcoin
 	let bitcoinMerge = await bitcoin.validate(request.body.origin, request.body.nonce)
